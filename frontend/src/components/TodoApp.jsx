f3e0ed2a74f8fdbbfdb7fe9f491e9131d8aa1ab2
shivanshiyadav <143997818+shivanshiyadav@users.noreply.github.com>
2023-11-21 12:44:31 +0530

    code

--- frontend/src/components/TodoApp.jsx
+++ frontend/src/components/TodoApp.jsx
@@ -7,13 +7,26 @@ const TodoApp = () => {
     const addTask = (e) => {
         if(e.code == 'Enter'){
             console.log(e.target.value);
-
             // adding new element to todo list
-            setTodolist( [ e.target.value ,  ...todolist  ] );
-            console.log( [ e.target.value ,  ...todolist  ] );
+            setTodolist( [ {text : e.target.value, completed: false} ,  ...todolist  ] );
+            console.log( [ {text : e.target.value, completed: false} ,  ...todolist  ] );
 
             e.target.value = "";
         }
+    }
+
+    const completeTask = (index) => {
+        console.log(index);
+        const temp = todolist;
+        temp[index].completed = true;
+        setTodolist([...temp]);
+        console.log(temp);
+    }
+
+    const deleteTask = (index) => {
+        const temp = todolist;
+        temp.splice(index, 1);
+        setTodolist([...temp]);
     }
 
   return (
@@ -27,10 +40,14 @@ const TodoApp = () => {
                     <input type="text" onKeyDown={ addTask } className='form-control' placeholder='Enter Task Here and Hit Enter ↲' />
                 </div>
                 <div className='card-body'>
-                    {todolist.map( ( task ) => { return <div className='d-flex justify-content-between p-3'>
-                        <p>{task.text}</p>
-                        <button className='btn btn-danger'>Delete</button>
-                    <button className='btn btn-primary'>Complete</button>
+                    {todolist.map( ( task, index ) => { return <div className='d-flex justify-content-between p-3 border border-2'>
+                        <p className={ task.completed ? 'text-success task-done' : 'fw-bold' }>{task.text}
+                          { task.completed ? <span className='badge text-bg-success ms-3'>Completed</span> : '' }
+                        </p>
+                        <div>
+                            <button className='btn btn-primary me-3' onClick={() => { completeTask(index) }} >Complete</button>
+                            <button className='btn btn-danger' onClick={() => { deleteTask(index) }} >Delete</button>
+                        </div>
                     </div> } ) }
                 </div>
             </div>
