00ad2335fa6d8f71e5a01e32d674164de76c57b5
shivanshiyadav <143997818+shivanshiyadav@users.noreply.github.com>
2023-11-23 12:52:47 +0530

    code

--- frontend/src/components/postData.js
+++ frontend/src/components/postData.js
@@ -1,25 +1,67 @@
-const postData =[
-{
-    
-    author : "Raju Rastogi",
-    postedOn : new Date(),
-    content: "tilli tilli tilli tilli ",
-    image: "https://helios-i.mashable.com/imagery/articles/02Ml12Tnr1wIjBQeoSNS3ss/hero-image.fill.size_1200x900.v1623370444.jpg",
-    comment :47,
-    likes: 92,
-    share: 17,
-
-},
-{
-    
-    author : "Raju Rastogi",
-    postedOn : new Date(),
-    content: "tilli tilli tilli tilli ",
-    image:" https://i.pinimg.com/originals/7a/c0/5c/7ac05cf6e27b6c022505a8209b99373a.png",
-    comment :47,
-    likes: 92,
-    share: 17,
-
-}
+const postData = [
+    {
+        author: "Raju Rastogi",
+        postedOn: new Date(),
+        content: "Content of the post goes here...",
+        image: 'https://media.licdn.com/dms/image/sync/D5622AQH_0RqPkQMm1w/feedshare-shrink_2048_1536/0/1699968204059?e=1703721600&v=beta&t=GYDOPrBRaCTH5blnSLZIe4vDEf6OK1QMDome9eUeubQ',
+        like: 14,
+        comment: 20,
+        share: 2
+    },
+    {
+        author: "Ramu",
+        postedOn: new Date(),
+        content: "Content of the post goes here...",
+        image: 'https://media.licdn.com/dms/image/sync/D5622AQH_0RqPkQMm1w/feedshare-shrink_2048_1536/0/1699968204059?e=1703721600&v=beta&t=GYDOPrBRaCTH5blnSLZIe4vDEf6OK1QMDome9eUeubQ',
+        like: 14,
+        comment: 20,
+        share: 2
+    },
+    {
+        author: "Raju Rastogi",
+        postedOn: new Date(),
+        content: "Content of the post goes here...",
+        image: 'https://media.licdn.com/dms/image/sync/D5622AQH_0RqPkQMm1w/feedshare-shrink_2048_1536/0/1699968204059?e=1703721600&v=beta&t=GYDOPrBRaCTH5blnSLZIe4vDEf6OK1QMDome9eUeubQ',
+        like: 14,
+        comment: 20,
+        share: 2
+    },
+    {
+        author: "Raju Rastogi",
+        postedOn: new Date(),
+        content: "Content of the post goes here...",
+        image: 'https://media.licdn.com/dms/image/sync/D5622AQH_0RqPkQMm1w/feedshare-shrink_2048_1536/0/1699968204059?e=1703721600&v=beta&t=GYDOPrBRaCTH5blnSLZIe4vDEf6OK1QMDome9eUeubQ',
+        like: 14,
+        comment: 20,
+        share: 2
+    },
+    {
+        author: "Raju Rastogi",
+        postedOn: new Date(),
+        content: "Content of the post goes here...",
+        image: 'https://media.licdn.com/dms/image/sync/D5622AQH_0RqPkQMm1w/feedshare-shrink_2048_1536/0/1699968204059?e=1703721600&v=beta&t=GYDOPrBRaCTH5blnSLZIe4vDEf6OK1QMDome9eUeubQ',
+        like: 14,
+        comment: 20,
+        share: 2
+    },
+    {
+        author: "Raju Rastogi",
+        postedOn: new Date(),
+        content: "Content of the post goes here...",
+        image: 'https://media.licdn.com/dms/image/sync/D5622AQH_0RqPkQMm1w/feedshare-shrink_2048_1536/0/1699968204059?e=1703721600&v=beta&t=GYDOPrBRaCTH5blnSLZIe4vDEf6OK1QMDome9eUeubQ',
+        like: 14,
+        comment: 20,
+        share: 2
+    },
+    {
+        author: "Raju Rastogi",
+        postedOn: new Date(),
+        content: "Content of the post goes here...",
+        image: 'https://media.licdn.com/dms/image/sync/D5622AQH_0RqPkQMm1w/feedshare-shrink_2048_1536/0/1699968204059?e=1703721600&v=beta&t=GYDOPrBRaCTH5blnSLZIe4vDEf6OK1QMDome9eUeubQ',
+        like: 14,
+        comment: 20,
+        share: 2
+    },
 ]
+
 export default postData;
