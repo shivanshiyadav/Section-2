9c95edb03b4a9123d3c83b3b86e4746bd75f1eff
shivanshiyadav <143997818+shivanshiyadav@users.noreply.github.com>
2023-12-13 13:03:45 +0530

    code

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div><h1>Welcome to Home Page </h1></div>
+    <div className='conatainer'>
+       <h1 className='home'>Welcome to  Our Home Page </h1>
+      <img className='img' src="https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+     </div>
   )
 }
 
