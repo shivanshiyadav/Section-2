2d33573942c909ddfa8f5b99c279408105e20bc8
shivanshiyadav <143997818+shivanshiyadav@users.noreply.github.com>
2023-12-18 00:30:22 +0530

    code

--- frontend/src/components/ManageUser.jsx
+++ frontend/src/components/ManageUser.jsx
@@ -4,74 +4,78 @@ import { Link } from 'react-router-dom';
 
 const ManageUser = () => {
 
-  const [userArray, setUseArray] = useState([]);
+  const [userArray, setUserArray] = useState([]);
 
-  const getUserData = async () => {
-    const res = await fetch('http://localhost:5000/user/getall');
-    console.log(res.status);
+    const getUserData = async () => {
 
-    const data = await res.json();
-    console.table(data);
-    setUseArray(data)
+        const res = await fetch('http://localhost:5000/user/getall');
+        console.log(res.status);
 
-  }
-  useEffect(() => {
-    getUserData();
+        const data = await res.json();
+        console.table(data);
 
-  }, []);
-
-  const deleteUser =  async(id) => {
-    console.log(id);
-
-    const res = await fetch('http://localhost:5000/user/delete/'+id, {
-      method:'DELETE',
-
-    });
-    console.log(res.status);
-    getUserData();
-    toast.success('User Delete Sucessfully');
-  }
-  const displayUserData = () => {
-    if (userArray.length) {
-      return userArray.map((user) => {
-        return <tr>
-          <td>{user._id}</td>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td>{user.location}</td>
-          <td>{user.password}</td>
-          <td>{user.delete}</td>
-          <td>
-             <button onClick={() => { deleteUser(user._id) }} className='btn btn-danger'>Delete User</button>
-          </td>
-          <td>
-             <Link to ={'/updateuser/'+user._id} className='btn btn-primary'>Update</Link>
-          </td>
-        </tr>
-      })
+        setUserArray(data);
     }
-  }
+    
+    useEffect(() => {
+      getUserData();
+    }, []);
+
+    const deleteUser = async (id) => {
+      console.log(id);
+
+      const res = await fetch('http://localhost:5000/user/delete/'+id, {
+        method: 'DELETE',
+      });
+
+      console.log(res.status);
+      getUserData();
+      toast.success('User Deleted Successfully');
+
+    }
+    
+    const displayUsersData = () => {
+      if(userArray.length){
+        return userArray.map((user) => {
+          return <tr>
+            <td>{user._id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.location}</td>
+            <td>{user.password}</td>
+            <td>
+              <button onClick={ () => { deleteUser(user._id) } } className='btn btn-danger'>Delete User</button>
+            </td>
+            <td>
+              <Link to={'/updateuser/'+user._id} className='btn btn-primary'>Update User</Link>
+            </td>
+          </tr>
+        })
+      }
+    }
+
   return (
     <div>
-      <div className="container">
-        <h1 className="text-center">Manage User</h1>
+      <div className='container'>
+        <h1 className='text-center'>Manage User Data</h1>
+        <hr />
+
         <table className='table table-dark'>
           <thead>
-            <tr>
-              <th>ID</th>
-              <th>NAME</th>
-              <th>EMAIL</th>
-              <th>LOCATION</th>
-              <th>PASSWORD</th>
-              <th>Delete</th>
-              <th colSpan={2}>Action</th>
-
-            </tr>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Location</th>
+                <th>Password</th>
+                <th colSpan={2}>Actions</th>
+              </tr>
           </thead>
           <tbody>
-            {displayUserData()}
+              {displayUsersData()}
           </tbody>
         </table>
+
       </div>
     </div>
   )
--- frontend/src/components/UpdateUser.jsx
+++ frontend/src/components/UpdateUser.jsx
@@ -1,22 +1,27 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
-const UpdateUser = () => {
-  const { id } = useParams();
 
-  const getUserData = async () => {
-    const res = await fetch('http://localhost:5000/user/getbyid/'+id);
-    console.log(res.status);
-    const data = await res.json();
-    console.log(data);
-  }
-  useEffect(() => {
-    getUserData();
-  }, [])
+const UpdateUser = () => {
+
+    const {id} = useParams();
+
+    const getUserData = async () => {
+        const res = await fetch('http://localhost:5000/user/getbyid/'+id);
+         console.log(res.status);
+        const data = await res.json();
+        console.log(data);
+    }
+
+    useEffect(() => {
+      getUserData();
+    }, [])
+    
+
   return (
     <div>
-      <h1>{id}</h1>
+        <h1>{id}</h1>
     </div>
   )
 }
 
-export default UpdateUser
+export default UpdateUser;
