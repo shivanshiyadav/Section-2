3d5c80f32a09deff127ed55a14d14fffa32a8f49
shivanshiyadav <143997818+shivanshiyadav@users.noreply.github.com>
2023-12-18 13:22:05 +0530

    code

--- frontend/src/components/UpdateUser.jsx
+++ frontend/src/components/UpdateUser.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 const UpdateUser = () => {
 
     const {id} = useParams();
+
+const [userData,setUserData] =useState(null);
 
     const getUserData = async () => {
         const res = await fetch('http://localhost:5000/user/getbyid/'+id);
@@ -19,7 +21,40 @@ const UpdateUser = () => {
 
   return (
     <div>
-        <h1>{id}</h1>
+         <div>
+        <div className='container py-5'>
+        <div className="card shadow">
+            <div className="card-body p-5">
+              <h3 className='my-3 text-center'>Brand Logo</h3>
+              <h5 className='text-center text-muted'>Signup Form</h5>
+
+              <form onSubmit={signupForm.handleSubmit} >
+
+                <label htmlFor="name">Name</label>
+                <span className='text-danger ms-3'>{signupForm.touched.name && signupForm.errors.name}</span>
+                <input type="text" id='name' onChange={signupForm.handleChange} value={signupForm.values.name} className='form-control mb-4' />
+
+                <label htmlFor="email">Email Address</label>
+                <span className='text-danger ms-3'>{signupForm.touched.email && signupForm.errors.email}</span>
+                <input type="text" id='email' onChange={signupForm.handleChange} value={signupForm.values.email} className='form-control mb-4' />
+
+                <label htmlFor="password">Password</label>
+                <span className='text-danger ms-3'>{signupForm.touched.password && signupForm.errors.password}</span>
+                <input type="password" id='password' onChange={signupForm.handleChange} value={signupForm.values.password} className='form-control mb-4' />
+
+                <label htmlFor="confirm">Confirm Password</label>
+                <span className='text-danger ms-3'>{signupForm.touched.confirm && signupForm.errors.confirm}</span>
+                <input type="password" id='confirm' onChange={signupForm.handleChange} value={signupForm.values.confirm} className='form-control mb-4' />
+
+
+                <button type='submit' className='btn btn-primary w-100 my-4'>Submit</button>
+
+              </form>
+
+            </div>
+          </div>
+        </div>
+    </div>
     </div>
   )
 }
