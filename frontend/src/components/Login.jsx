4cf7dd4aca6bf241543bcb52826e05735cd03145
shivanshiyadav <143997818+shivanshiyadav@users.noreply.github.com>
2023-12-23 12:54:32 +0530

    code

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -65,49 +65,10 @@ const Login = () => {
   return (
     <div className='background'> 
 
-    <div style={{ fontFamily: 'Montserrat' }}>
-      <div className='container text-start mt-4'>
-        <div className='row' >
-          <div className='col d-flex align-items-center justify-content-center' style={{backgroundColor: '#391b7f', color: 'white'}}>
-            <div className='card display-1 border-0 '>
-              <div className='card-body text-white' style={{backgroundColor: '#391b7f', }}>
-                Welcome
-                <br />
-                Back !!
-              </div>
-            </div>
-          </div>
-          <div className='col'style={{backgroundColor: '#d9c0c1'}}>
-            <div className="card border-0" style={{backgroundColor: '#d9c0c1'}}>
-              <div className="card-body" >
-
-                <h3 className='my-3 text-center display-5 fw-bold'>Login</h3>
-
-                <p className='text-center my-4 fw-medium'>Welcome Back..!! Please login into your account.</p>
-
-                <form onSubmit={loginForm.handleSubmit}>
-
-                  <label className='fw-medium' htmlFor="email">Email Address</label>
-                  <span className='text-danger ms-3'>{loginForm.touched.email && loginForm.errors.email}</span>
-                  <input type="text" id='email' onChange={loginForm.handleChange} value={loginForm.values.email} className='form-control mb-4' style={{borderWidth:'2px' ,borderColor: '#391b7f'}}/>
-
-                  <label className='fw-medium' htmlFor="password">Password</label>
-                  <span className='text-danger ms-3'>{loginForm.touched.password && loginForm.errors.password}</span>
-                  <input type="password" id='password' onChange={loginForm.handleChange} value={loginForm.values.password} className='form-control mb-4' style={{borderWidth:'2px' ,borderColor: '#391b7f'}}/>
-
-                  <button type='submit' className='btn btn-primary w-100 my-4 border-0' style={{backgroundColor: '#391b7f', color:'white'}}>Submit</button>
-
-                </form>
-
-                <p className='fw-medium mb-5 mt-2'>Already a User ? <a href="Login" style={{color:'#391b7f'}}>Login</a></p>
-
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
+    <div className="container">
+      
     </div>
+     
     </div>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">
       Navbar
